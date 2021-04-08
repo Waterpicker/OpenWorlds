75c440a75a05705b479feb0e3c4e046348071642
Mysticpasta1 <ethan.cantrell@asub.edu>
2021-04-08 04:37:58 -0500

    Updated to 1.17 snapshots
    Fixed the one freaking broke mixin.

--- src/main/java/io/github/waterpicker/openworlds/renderer/CloudRenderer.java
+++ src/main/java/io/github/waterpicker/openworlds/renderer/CloudRenderer.java
@@ -2,7 +2,8 @@ package io.github.waterpicker.openworlds.renderer;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Matrix4f;
 
 public interface CloudRenderer {
-    void render(MinecraftClient client, MatrixStack matrices, float tickDelta, double cameraX, double cameraY, double cameraZ);
+    void render(MinecraftClient client, MatrixStack matrices, Matrix4f matrix4f, float tickDelta, double cameraX, double cameraY, double cameraZ);
 }
